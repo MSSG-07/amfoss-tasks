5f817891ea46d3e8fe2c90844e1f622f40cdbd82
MSSG-07 <mssuryagayathri4076@gmail.com>
2025-01-07 15:41:08 +0530

    Path updated

--- Task2/PixelatedProblemSolver.py
+++ Task2/PixelatedProblemSolver.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
-img = Image.open("/home/m-s-surya-gayathri/Pictures/Screenshots/5+5.png")
+img = Image.open("amfoss-tasks/Task2/5+5.png")
 ex = pytesseract.image_to_string(img).strip()
 
 print(f"Extracted expression: '{ex}'")
